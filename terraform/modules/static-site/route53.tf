20f90914f6851d42d0511b80ac2d2a694823eba9
Spencer Williams <s@spencerenglish.com>
2021-09-14 10:02:09 -0700

    typo

--- terraform/modules/static-site/route53.tf
+++ terraform/modules/static-site/route53.tf
@@ -1,16 +1,16 @@
 # Route 53
 resource "aws_route53_zone" "site" {
-  count = len(var.hosted_zone_id) > 0 ? 0 : 1
+  count = length(var.hosted_zone_id) > 0 ? 0 : 1
   name = var.domain_name
 }
 
 data "aws_route53_zone" "site" {
-  count = len(var.hosted_zone_id) > 0 ? 1 : 0
+  count = length(var.hosted_zone_id) > 0 ? 1 : 0
   zone_id = var.hosted_zone_id
 }
 
 locals {
-  zone_id = len(var.hosted_zone_id) > 0 ? data.aws_route53_zone.site.zone_id : aws_route53_zone.site.zone_id
+  zone_id = length(var.hosted_zone_id) > 0 ? data.aws_route53_zone.site.zone_id : aws_route53_zone.site.zone_id
 }
 
 resource "aws_route53_record" "site" {
