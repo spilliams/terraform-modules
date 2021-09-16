5fbfeda7439ee5b0d0b1a5e765ff3c06daf8e042
Spencer Williams <s@spencerenglish.com>
2021-09-15 21:57:03 -0700

    buckets don't need a slug, they're already based on something semi-unique

--- terraform/modules/static-site/inputs.tf
+++ terraform/modules/static-site/inputs.tf
@@ -1,6 +1,6 @@
 variable "domain_name" {
   description = "This is the name of the domain you wish to use for this static site"
-  type = string
+  type        = string
 }
 
 variable "acm_certificate_arn" {
@@ -9,18 +9,18 @@ variable "acm_certificate_arn" {
 
 variable "extra_aliases" {
   description = "(Optional) These are the extra aliases you wish to apply to the cloudfront"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "extra_allowed_origins" {
   description = "(Optional) These are the allowed origins on the S3 bucket (CORS rules)"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "hosted_zone_id" {
   description = "(Optional) This is for users who want to create their own hosted zone. Note that this module will create an A record on the hosted zone"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
--- terraform/modules/static-site/main.tf
+++ terraform/modules/static-site/main.tf
@@ -1,10 +1,4 @@
-resource "random_string" "slug" {
-  length  = 6
-  upper   = false
-  special = false
-}
-
 locals {
   bucket_domain_name = join("-", split(".", var.domain_name))
-  bucket_name        = "tf-${local.bucket_domain_name}-${random_string.slug.result}"
+  bucket_name        = "tf-${local.bucket_domain_name}"
 }
